acb8e4ee7b1264c6d9c2e67a64b145c878c81caa
Manish Goyal <manishgoyalbsr7@gmail.com>
2022-06-19 21:02:31 +0530

    :)

--- Array/46-Reverse Pairs.cpp
+++ Array/46-Reverse Pairs.cpp
@@ -1,26 +1,67 @@
 // Problem:  493. Reverse Pairs
 /*-------------------------------------------------------------------------------------*/
-// Approach 1 :
+// Approach 1 : Merge Sort
 #include <bits/stdc++.h>
 using namespace std;
 
-int mergesort(vector<int> &nums, int start, int end){
-    if(start==end){
-        return 0;
+void Merge(vector<int> &nums, int low, int mid, int high, int &count) {
+    int total = 0;
+    int j = mid + 1;
+    for (int i = low; i <= mid; i++) {
+        while (j <= high && nums[i] > 2LL * nums[j]) {
+            j++;
+            total++;
+        }
+        count += total;
     }
-    int mid=start+(end-start)/2;
-    mergesort(nums, start, mid);
-    mergesort(nums, mid+1, end);
-    merge(nums, start, mid, end);
+
+    vector<int> temp;
+    int left = low, right = mid + 1;
+    while (left <= mid && right <= high) {
+        if (nums[left] <= nums[right]) {
+            temp.push_back(nums[left]);
+            left++;
+        } else {
+            temp.push_back(nums[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(nums[left]);
+        left++;
+    }
+    while (right <= high) {
+        temp.push_back(nums[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        nums[i] = temp[i - low];
+    }
 }
 
-int reversePairs(vector<int>nums) {
-    cout << mergesort(nums, 0, nums.size()-1) << endl;
+void MergeSort(vector<int> &nums, int low, int high, int &count) {
+    if (low >= high) {
+        return;
+    }
+    int mid = (low + high) / 2;
+    MergeSort(nums, low, mid, count);
+    MergeSort(nums, mid + 1, high, count);
+    Merge(nums, low, mid, high, count);
+}
+
+int reversePairs(vector<int> &nums) {
+    int count = 0;
+    MergeSort(nums, 0, nums.size() - 1, count);
+    return count;
 }
 
 int main() {
+    vector<int> nums = {1, 3, 2, 3, 1};
+    cout << reversePairs(nums) << endl;
     return 0;
 }
-// Time Complexity:  O()
-// Auxillary Space Complexity:  O()
+// Time Complexity:  O(NlogN)
+// Auxillary Space Complexity:  O(1)
 /*-------------------------------------------------------------------------------------*/
--- Array/47-Search in 2d sorted matrix.cpp
+++ Array/47-Search in 2d sorted matrix.cpp
@@ -1,11 +1,13 @@
-// Problem:
+// Problem:  74. Search a 2D Matrix
 /*-------------------------------------------------------------------------------------*/
-// Approach 1 :
+// Approach 1 : Binary Search + Mathematics
 #include <bits/stdc++.h>
 using namespace std;
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    if (!matrix.size()) return false;
+    if (matrix.size() == 0 || matrix[0].size() == 0) {
+        return false;
+    }
     int n = matrix.size();
     int m = matrix[0].size();
     int lo = 0;
@@ -25,10 +27,10 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
 }
 
 int main() {
-    vector<vector<int>>matrix= {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    vector<vector<int>> matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
     int target = 7;
     return 0;
 }
-// Time Complexity:  O()
-// Auxillary Space Complexity:  O()
+// Time Complexity:  O((M*N)log(M*N))
+// Auxillary Space Complexity:  O(1)
 /*-------------------------------------------------------------------------------------*/
